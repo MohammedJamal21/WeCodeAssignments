955b384c8d136db684340f573b0d95de9b08a721
7amaa <mojamal.it@gmail.com>
2022-07-13 18:56:42 +0300

    Assignment7: Added an rounded image & styled text

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text("Hello Kurdistan"),
+        backgroundColor: const Color.fromARGB(58, 92, 164, 72),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(205, 55, 161, 26),
+          title: const Center(
+            child: Text(
+              "Kurdistan",
+            ),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(
+                vertical: 10,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  'https://gov.krd/media/2897/kurdistan_flag_3457x2355.gif',
+                  width: 400,
+                ),
+              ),
+            ),
+            const Text(
+              'Hello Kurdistan',
+              style: TextStyle(
+                color: Color.fromARGB(205, 55, 161, 26),
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
         ),
       ),
     );

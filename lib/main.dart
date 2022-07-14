947b74641fdc1bde7ef7ebe55e24181470998194
7amaa <mojamal.it@gmail.com>
2022-07-14 18:59:38 +0300

    Assignment8: Added 3 images, you can switch them..

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,21 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String firstApe =
+      'https://pbs.twimg.com/media/FLGUpWKXwAQYaY7?format=jpg&name=small';
+  String secondApe =
+      'https://static.cryptobriefing.com/wp-content/uploads/2022/01/31074857/bored-ape-unnamed-1-440x440.png';
+  String thirdApe =
+      'https://preview.redd.it/kzdbqcc7jyf81.png?auto=webp&s=3e6cd10238a625f552c7b408f197d256c5807af7';
+  String currentApe = '';
 
   @override
   Widget build(BuildContext context) {
@@ -13,41 +26,96 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(58, 92, 164, 72),
         appBar: AppBar(
-          backgroundColor: const Color.fromARGB(205, 55, 161, 26),
           title: const Center(
-            child: Text(
-              "Kurdistan",
-            ),
+            child: Text('Choose an image:'),
           ),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              alignment: Alignment.center,
-              margin: const EdgeInsets.symmetric(
-                vertical: 10,
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: Image.network(
-                  'https://gov.krd/media/2897/kurdistan_flag_3457x2355.gif',
-                  width: 400,
-                ),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: currentApe == ''
+                        ? Image.network(
+                            firstApe,
+                            height: 340,
+                            width: 340,
+                          )
+                        : Image.network(
+                            currentApe,
+                            height: 340,
+                            width: 340,
+                          ),
+                  ),
+                )
+              ],
             ),
-            const Text(
-              'Hello Kurdistan',
-              style: TextStyle(
-                color: Color.fromARGB(205, 55, 161, 26),
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 3,
-                fontStyle: FontStyle.italic,
-              ),
+            //----------------------------------------------------------------------------------------
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          currentApe = firstApe;
+                        });
+                      },
+                      child: Image.network(
+                        firstApe,
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          currentApe = secondApe;
+                        });
+                      },
+                      child: Image.network(
+                        secondApe,
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          currentApe = thirdApe;
+                        });
+                      },
+                      child: Image.network(
+                        thirdApe,
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ),
+                ), 
+              ],
             ),
           ],
         ),
